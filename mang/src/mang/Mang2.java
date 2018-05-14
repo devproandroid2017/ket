a16aa1210f6870871f7440a39af8c9ee223c2924
buike <buike@DESKTOP-HUBJA7U>
2018-05-14 20:27:13 +0700

    mang moi

--- mang/src/mang/Mang2.java
+++ mang/src/mang/Mang2.java
@@ -1,6 +1,21 @@
 package mang;
 
+import java.util.Arrays;
+
 public class Mang2 {
+	//Xóa phần tử trong mảng
+	public static int[] removeElement(int[] array, int removeIndex) {
+		int [] newArray = new int[array.length-1];
+		
+		for(int i =removeIndex;i<array.length-1;i++) {
+			array[i] = array[i+1];
+		}
+		
+		for (int i = 0; i<array.length-1;i++) {
+			newArray[i] = array[i];
+		}
+		return newArray;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -13,6 +28,12 @@ public class Mang2 {
 		 }  
 		 System.out.println();  
 		}  
+		
+		int[] intArray = {1,2,3,4,5,6};
+		
+		
+		System.out.println(Arrays.toString(removeElement(intArray, 3)));
+		
 	}
 
 }
